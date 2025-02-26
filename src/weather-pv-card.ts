bd18de4d3a43227a46139e64accdc0eeed44767d
Ondrej Salplachta <ondrej@salplachta.net>
2025-02-26 16:16:24 +0100

    feat: Added hourly forecasts

--- src/weather-pv-card.ts
+++ src/weather-pv-card.ts
@@ -46,6 +46,12 @@ export class WeatherPvCard extends LitElement implements LovelaceCard {
     @state() private _selectedDay?: dayjs.Dayjs;
     private _updateTimer?: number;
 
+    constructor() {
+        super();
+        //dayjs.locale('cs');
+        dayjs.locale(this._hass?.locale?.language ?? 'en')
+    }
+
     public set hass(value: HomeAssistant) {
         const hassWasSet = this._hass !== undefined;
         this._hass = value;
@@ -108,14 +114,20 @@ export class WeatherPvCard extends LitElement implements LovelaceCard {
             justify-content: space-between;
             padding: 16px;
         }
-        .forecast-day {
+        .forecast-container-hourly{
+            overflow: auto;
+        }
+        .forecast-interval {
             display: flex;
             flex-direction: column;
             align-items: center;
             flex: 1;
             border-right:  0.5px solid rgb(47, 52, 60);
         }
-        .forecast-bar {
+        .forecast-interval-daily{
+            cursor: pointer;
+        }
+        .forecast-values {
             //height: 100px;
             margin-top: 8px;
             width: 100%;
@@ -127,6 +139,15 @@ export class WeatherPvCard extends LitElement implements LovelaceCard {
         .day-name{
             font-size: 1.2em;
             font-weight: bold;
+        }
+        .forecast-hourly-header{
+            display: flex;
+            justify-content: flex-start;
+            padding: 16px;
+            gap: 16px;
+            font-size: 1.2em;
+            font-weight: bold;
+            cursor: pointer;
         }
     `
 
@@ -142,71 +163,126 @@ export class WeatherPvCard extends LitElement implements LovelaceCard {
         if (!this._hass?.states[this.config.pv_entity]) {
             return `Entity ${this.config.pv_entity} not found`;
         }
-        
+
         if (!this._forecasts) {
             return "Loading...";
         }
 
-        const dailyForecast = this._forecasts.weatherDaily;
-        const pvForecast = this._forecasts.pv;
 
         return html`
             <ha-card>
-                <div class="forecast-container">
-                    ${dailyForecast.map(day => {
-                        const pvData = pvForecast.find(pv =>pv.time.format('YYYY-MM-DD') === dayjs(day.datetime).format('YYYY-MM-DD'));
-
-                        return html`
-                            <div class="forecast-day">
-                                <div class="day-name">${day.datetime.locale(this._hass?.locale?.language ?? 'en').format('ddd')}</div>
-                                <div>${day.datetime.format('D.')}</div>
-                                <ha-icon icon=${weatherIconsHassNative[day.condition as keyof typeof weatherIconsHassNative]}></ha-icon>
-                                <div class="forecast-bar">
-                                    <weather-value-column 
-                                        .value=${day.temperature}
-                                        .allValues=${dailyForecast.map(d => d.temperature)}
-                                        color="#77450D"
-                                        units="°C"
-                                    ></weather-value-column>
-                                    <weather-value-column 
-                                        .value=${day.templow}
-                                        .allValues=${dailyForecast.map(d => d.templow)}
-                                        color="#2F343C"
-                                        units="°C"
-                                    ></weather-value-column>
-                                    <weather-value-column 
-                                        .value=${day.precipitation}
-                                        .allValues=${dailyForecast.map(d => d.precipitation)}
-                                        color="#0C5174"
-                                        units="mm"
-                                        fontSizeRatio=80
-                                    ></weather-value-column>
-                                    <weather-value-column 
-                                        .value=${day.wind_speed}
-                                        .allValues=${dailyForecast.map(d => d.wind_speed)}
-                                        color="#004D46"
-                                        units="km/h"
-                                        fontSizeRatio=80
-                                    ></weather-value-column>                        
-                                    ${pvData ? html`
-                                        <weather-value-column
-                                            .value=${pvData.power}
-                                            .allValues=${pvForecast.map(d => d.power)}
-                                            color="#5C4405"
-                                            units="kWh"
-                                            fontSizeRatio=80
-                                            ></weather-value-column>
-                                    ` : ''}
-                                </div>
-                            </div>
-                        `;
-                    })}
-                </div>
+                    ${this._selectedDay ?
+                this.renderHours(this._forecasts.weatherHourly)
+                :
+                this.renderDays(this._forecasts.weatherDaily, this._forecasts.pv)
+            }
             </ha-card>
         `
     }
 
+    private renderDays(dailyForecast: WeatherForecast[], pvForecast: PvForecast[]) {
+        return html` 
+            <div class="forecast-container">
+                ${dailyForecast.map(day => {
+                    const pvData = pvForecast.find(pv => pv.time.format('YYYY-MM-DD') === dayjs(day.datetime).format('YYYY-MM-DD'));
+                    return html`
+                        <div class="forecast-interval forecast-interval-daily" @click=${() => this._selectedDay = day.datetime}>
+                            <div class="day-name">${day.datetime.format('ddd')}</div>
+                            <div>${day.datetime.format('D.')}</div>
+                            <ha-icon icon=${weatherIconsHassNative[day.condition as keyof typeof weatherIconsHassNative]}></ha-icon>
+                            <div class="forecast-values">
+                                <weather-value-column 
+                                    .value=${day.temperature}
+                                    .allValues=${dailyForecast.map(d => d.temperature)}
+                                    color="#77450D"
+                                    units="°C"
+                                ></weather-value-column>
+                                <weather-value-column 
+                                    .value=${day.templow}
+                                    .allValues=${dailyForecast.map(d => d.templow)}
+                                    color="#2F343C"
+                                    units="°C"
+                                ></weather-value-column>
+                                <weather-value-column 
+                                    .value=${day.precipitation}
+                                    .allValues=${dailyForecast.map(d => d.precipitation)}
+                                    color="#0C5174"
+                                    units="mm"
+                                    fontSizeRatio=80
+                                ></weather-value-column>
+                                <weather-value-column 
+                                    .value=${day.wind_speed}
+                                    .allValues=${dailyForecast.map(d => d.wind_speed)}
+                                    color="#004D46"
+                                    units="km/h"
+                                    fontSizeRatio=80
+                                ></weather-value-column>                        
+                                ${pvData ? html`
+                                    <weather-value-column
+                                        .value=${pvData.power}
+                                        .allValues=${pvForecast.map(d => d.power)}
+                                        color="#5C4405"
+                                        units="kWh"
+                                        fontSizeRatio=80
+                                        ></weather-value-column>
+                                ` : ''}
+                            </div>
+                        </div>
+                    `;
+                    })}
+            </div>
+            `
+        
+     
+    }
+    private renderHours(hourlyForecast: WeatherForecast[]) {
+        const forecast = hourlyForecast.filter(hour => hour.datetime.format('YYYY-MM-DD') === this._selectedDay?.format('YYYY-MM-DD'));
+        if (!forecast) {
+            return html`<div>No data for this day</div>`;
+        }
+
+        return html`
+            <div>
+                <div class="forecast-hourly-header" @click=${() => this._selectedDay = undefined}>
+                    <div>←</div>
+                    <div>${this._selectedDay?.format('dddd D.M.')}</div>
+                </div>
+                <div class="forecast-container forecast-container-hourly">
+                    ${forecast.map(hour => html`
+                        <div class="forecast-interval">
+                            <div>${hour.datetime.format('HH')}</div>
+                            <ha-icon icon=${weatherIconsHassNative[hour.condition as keyof typeof weatherIconsHassNative]}></ha-icon>
+                            <div class="forecast-values">
+                                <weather-value-column 
+                                    .value=${hour.temperature}
+                                    .allValues=${hourlyForecast.map(d => d.temperature)}
+                                    color="#77450D"
+                                    units="°C"
+                                ></weather-value-column>
+                                <weather-value-column 
+                                    .value=${hour.precipitation}
+                                    .allValues=${hourlyForecast.map(d => d.precipitation)}
+                                    color="#0C5174"
+                                    units="mm"
+                                    fontSizeRatio=80
+                                ></weather-value-column>
+                                <weather-value-column 
+                                    .value=${hour.wind_speed}
+                                    .allValues=${hourlyForecast.map(d => d.wind_speed)}
+                                    color="#004D46"
+                                    units="km/h"
+                                    fontSizeRatio=80
+                                ></weather-value-column>
+                            </div>
+                        </div>
+                    `)}
+                </div>
+            </div>
+            `
+    }
 }
+
+
 
 (window as any).customCards = (window as any).customCards || [];
 (window as any).customCards.push({
